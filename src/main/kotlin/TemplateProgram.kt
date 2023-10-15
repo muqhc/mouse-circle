f296b88fd8b245c967ede03505acb05ed4f1f818
muqhc <88622655+muqhc@users.noreply.github.com>
2023-10-15 10:39:18 +0900

    syncronized

--- src/main/kotlin/TemplateProgram.kt
+++ src/main/kotlin/TemplateProgram.kt
@@ -14,41 +14,35 @@ import kotlin.math.*
 
 
 
-suspend fun main() = applicationAsync {
-    program {
-
-        var drawPosition: Vector2 = Vector2(width/2.0,height/2.0)
-
-        //mouse.moved.listen {
-        //    drawPosition = it.position
-        //}
-
-        val composite = compose {
-            draw {
-                drawer.fill = ColorRGBa.PINK
-                drawer.stroke = null
-                drawer.circle(width / 2.0, height / 2.0, 175.0)
-            }
-            
-            layer {
-                blend(Add()) {
-                    clip = true
-                }
+fun main() {
+    application {
+        program {
+            val composite = compose {
                 draw {
                     drawer.fill = ColorRGBa.PINK
                     drawer.stroke = null
-                    drawer.circle(width / 2.0, height / 2.0 + 100.0, 100.0)
+                    drawer.circle(width / 2.0 + sin(seconds * 2) * 100.0, height / 2.0, 175.0)
                 }
-                post(ApproximateGaussianBlur()) {
-                    window = 25
-                    sigma = 10.00
+            
+                layer {
+                    blend(Add()) {
+                        clip = true
+                    }
+                    draw {
+                        drawer.fill = ColorRGBa.PINK
+                        drawer.stroke = null
+                        drawer.circle(width / 2.0, height / 2.0 + cos(seconds * 2) * 100.0, 100.0)
+                    }
+                    post(ApproximateGaussianBlur()) {
+                        // -- this is actually a function that is called for every draw
+                        window = 25
+                        sigma = cos(seconds) * 10.0 + 10.01
+                    }
                 }
             }
-        }
-
-        extend {
-            drawer.clear(ColorRGBa.BLACK)
-    
+            extend {
+                composite.draw(drawer)
+            }
         }
     }
 }

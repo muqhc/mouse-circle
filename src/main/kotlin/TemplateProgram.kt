ae7817c336d767bcfedbc6f36d9b5729a8428c3f
muqhc <88622655+muqhc@users.noreply.github.com>
2023-10-19 14:15:15 +0900

    remove composite

--- src/main/kotlin/TemplateProgram.kt
+++ src/main/kotlin/TemplateProgram.kt
@@ -16,31 +16,26 @@ import kotlin.math.*
 
 suspend fun main() = applicationAsync {
     program {
-        val composite = compose {
-            draw {
-                drawer.fill = ColorRGBa.PINK
-                drawer.stroke = null
-                drawer.circle(width / 2.0 + sin(seconds * 2) * 100.0, height / 2.0, 175.0)
-            }
-        
-            layer {
-                blend(Add()) {
-                    clip = true
-                }
-                draw {
-                    drawer.fill = ColorRGBa.PINK
-                    drawer.stroke = null
-                    drawer.circle(width / 2.0, height / 2.0 + cos(seconds * 2) * 100.0, 100.0)
-                }
-                post(ApproximateGaussianBlur()) {
-                    // -- this is actually a function that is called for every draw
-                    window = 25
-                    sigma = cos(seconds) * 10.0 + 10.01
-                }
-            }
+        var mousePosition: Vector2 = Vector2(0.0,0.0)
+
+        mouse.moved.listen {
+            mousePosition = it.position
         }
+
         extend {
-            composite.draw(drawer)
+            val myBackgroundColor = ColorRGBa.BLACK
+            val myPrimaryColor = rgb("#FC9601")
+            val windowSize = Vector2(width.toDouble(),height.toDouble())
+            
+            drawer.circles {
+                stroke = null
+
+                fill = myPrimaryColor
+                circle(mousePosition,150.0)
+
+                fill = myBackgroundColor
+                circle(windowSize - mousePosition,150.0)
+            }
         }
     }
 }

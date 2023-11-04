63c3160e7b652a3c79b070ce49f09171734ed98e
muqhc <88622655+muqhc@users.noreply.github.com>
2023-11-04 21:27:23 +0900

    fix

--- src/main/kotlin/TemplateProgram.kt
+++ src/main/kotlin/TemplateProgram.kt
@@ -24,7 +24,7 @@ suspend fun main() = applicationAsync {
 
         val primaryMsg = paramMap["primary"] ?: "welcome"
         val secondMsg = paramMap["second"] ?: "scroll down"
-        val scalePreset: Double = js("parseFloat(${paramMap["scale"]})") ?: 1.0
+        val scalePreset: Double = paramMap["scale"]?.toDoubleOrNull() ?: 1.0
 
         mouse.moved.listen {
             mousePosition = it.position
